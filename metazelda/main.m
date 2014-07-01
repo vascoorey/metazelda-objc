c344a37a7c61983b2dcb742739f725f778df72f2
Vasco Orey <vasco.orey@gmail.com>
2014-07-01 14:28:58 +0100

    Initial Commit

---
+++ metazelda/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  metazelda
+//
+//  Created by Vasco d'Orey on 01/07/14.
+//  Copyright (c) 2014 Delta Dog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DOGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([DOGAppDelegate class]));
+  }
+}
